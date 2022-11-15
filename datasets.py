360e616b5d6952121cf04ccb7a69e8392681b4ce
raysinensis <raysinensis@gmail.com>
2022-11-15 09:22:55 -0500

    general data loading

--- datasets.py
+++ datasets.py
@@ -5,6 +5,31 @@ from torch_geometric.data import InMemoryDataset, Data
 from sklearn.metrics import pairwise_distances
 import pandas as pd
 
+def load_data(labeled_file, unlabeled_file, distance_thres, sample_rate):
+    train_df = pd.read_csv(labeled_file)
+    test_df = pd.read_csv(unlabeled_file)
+    # train_df = train_df.sample(n=round(sample_rate*len(train_df)), random_state=1)
+    # train_df = train_df.loc[np.logical_and(train_df['tissue'] == 'CL', train_df['donor'] == 'B004')]
+    # test_df = test_df.loc[np.logical_and(test_df['tissue'] == 'CL', test_df['donor'] == 'B005')]
+    train_X = train_df.iloc[:, 1:49].values # node features, indexes depend on specific datasets
+    test_X = test_df.iloc[:, 1:49].values
+    labeled_pos = train_df.iloc[:, -6:-4].values # x,y coordinates, indexes depend on specific datasets
+    unlabeled_pos = test_df.iloc[:, -5:-3].values
+    labeled_regions = train_df['unique_region']
+    unlabeled_regions = test_df['unique_region']
+    train_y = train_df['cell_type_A'] # class information
+    cell_types = np.sort(list(set(train_df['cell_type_A'].values))).tolist()
+    # we here map class in texts to categorical numbers and also save an inverse_dict to map the numbers back to texts
+    cell_type_dict = {}
+    inverse_dict = {}    
+    for i, cell_type in enumerate(cell_types):
+        cell_type_dict[cell_type] = i
+        inverse_dict[i] = cell_type
+    train_y = np.array([cell_type_dict[x] for x in train_y])
+    labeled_edges = get_hubmap_edge_index(labeled_pos, labeled_regions, distance_thres)
+    unlabeled_edges = get_hubmap_edge_index(unlabeled_pos, unlabeled_regions, distance_thres)
+    return train_X, train_y, test_X, labeled_edges, unlabeled_edges, inverse_dict
+
 def get_hubmap_edge_index(pos, regions, distance_thres):
     # construct edge indexes when there is region information
     edge_list = []
